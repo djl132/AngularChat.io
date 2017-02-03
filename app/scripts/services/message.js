02b4d5008d3730f65e3bbc864a4ded9c1c9ed650
djl132 <djl132@case.edu>
2017-02-03 13:59:05 -0800

    vi

--- app/scripts/services/message.js
+++ app/scripts/services/message.js
@@ -1,16 +1,36 @@
 (function() {
   function Message($firebaseArray) {
     var ref = firebase.database().ref().child("messages");
-    var messages = $firebaseArray(ref);
     
-    return {
+    var Message = {
       currentMessages : null,
       getRoomById: function(roomId){
-        ref.orderByChild("roomId").equalTo(roomId).on("child_added", function(message){
-          this.currentMessages = messages;
+        // this === Message
+        ref.orderByChild("roomId").equalTo(roomId).on("value", function(data){
+          // on value returns an object containing query data
+          // val() property on data returns the actual object
+          var messages = data.val();
+                    
+//          // messagesArray will contain all of the messages in an array format
+//          var messagesArray = [];
+//          
+//          // turn the object into an array
+//          for (var messageId in messages) {
+//            messagesArray.push(messages[messageId]);
+//          }
+//          
+          // Set the property on the service that the view is binding to
+          // chat.message.currentMessages 
+          Message.currentMessages = messages;
+
+          // this === window, so we are creating a global variable called
+          // currentMessages, which is why this code did not work
+          //this.currentMessages = messages;
         });
       }
     };
+    
+    return Message;
   }
 
   angular
